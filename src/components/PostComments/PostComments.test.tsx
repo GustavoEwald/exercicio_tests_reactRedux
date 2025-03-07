9123d57a39ac1e411f4e7c16a50518815aaa7f74
Gustavo Ewald <ewaldgustavo@gmail.com>
2025-03-06 23:57:12 -0300

    Finalização do codigo de testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,40 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
+
+const primeiroComentario = 'Este é o primeiro comentário'
+const segundoComentario = 'Este é o segundo comentário'
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment/>)
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    test('Renderizar o primeiro comentário', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: primeiroComentario
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('Este é o primeiro comentário')).toBeInTheDocument()
+    })
+    test('Renderizar dois comentários', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: primeiroComentario
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: segundoComentario
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getAllByTestId('lista-comentarios')).toHaveLength(2)
+        expect(screen.getByText('Este é o primeiro comentário')).toBeInTheDocument()
+        expect(screen.getByText('Este é o segundo comentário')).toBeInTheDocument()
+    })
 });
